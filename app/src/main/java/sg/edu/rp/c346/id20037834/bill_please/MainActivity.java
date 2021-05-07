5756bb52134960e5317ed0995fc2bcdbbe3c3ef8
20037834 <20037834@myrp.edu.sg>
2021-05-07 14:33:38 +0800

    Updated variable names to make it better.

--- app/src/main/java/sg/edu/rp/c346/id20037834/bill_please/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20037834/bill_please/MainActivity.java
@@ -3,35 +3,37 @@ package sg.edu.rp.c346.id20037834.bill_please;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-    EditText amount;
-    EditText pax;
-    EditText discount;
-    Button noSVC;
-    Button GST;
-    Button split;
-    Button reset;
-    RadioButton cash;
-    RadioButton payNow;
+    EditText etAmount;
+    EditText etPax;
+    EditText etDiscount;
+    Button btnNoService;
+    Button btnGST;
+    Button btnSplit;
+    Button btnReset;
+    RadioButton rbCash;
+    RadioButton rbPayNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        amount = findViewById(R.id.etAmountProvided);
-        pax = findViewById(R.id.etPaxNumGiven);
-        discount = findViewById(R.id.etDiscountGiven);
-        noSVC = findViewById(R.id.btnServiceCharge);
-        GST = findViewById(R.id.btnGST);
-        split = findViewById(R.id.btnSplit);
-        reset = findViewById(R.id.btnReset);
-        cash = findViewById(R.id.radiobtnCash);
-        payNow = findViewById(R.id.radiobtnPaynow);
+        etAmount = findViewById(R.id.etAmountProvided);
+        etPax = findViewById(R.id.etPaxNumGiven);
+        etDiscount = findViewById(R.id.etDiscountGiven);
+        btnNoService = findViewById(R.id.btnServiceCharge);
+        btnGST = findViewById(R.id.btnGST);
+        btnSplit = findViewById(R.id.btnSplit);
+        btnReset = findViewById(R.id.btnReset);
+        rbCash = findViewById(R.id.radiobtnCash);
+        rbPayNow = findViewById(R.id.radiobtnPaynow);
+
 
     }
 
